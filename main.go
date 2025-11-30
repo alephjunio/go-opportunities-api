e0d85f8f4e5973b25e8b838df0877e7bf18f7062
Alef Junio Pereira da Silva <alephjunio@gmail.com>
2025-11-30 01:23:19 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/alephjunio/go-opportunities-api/router"
+import (
+	"github.com/alephjunio/go-opportunities-api/config"
+	"github.com/alephjunio/go-opportunities-api/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config Initialization error: %v", err)
+		return
+	}
+
 	router.Inizialize()
 
 }
