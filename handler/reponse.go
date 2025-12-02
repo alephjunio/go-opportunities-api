8c64a0f1cf2a1d5c6fde1a5e18c029fb5885c92b
Alef Junio Pereira da Silva <alephjunio@gmail.com>
2025-12-02 20:09:02 -0300

    feat: Add request and response helpers

---
+++ handler/reponse.go
@@ -0,0 +1,25 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SendError(ctx *gin.Context, code int, menssage string) {
+	ctx.Header("content-type", "application/json")
+	ctx.JSON(code, gin.H{
+		"message": menssage,
+		"code":    code,
+	})
+}
+
+func SendSucess(ctx *gin.Context, op string, data interface{}) {
+	ctx.Header("content-type", "application/json")
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("operation from handler: %s success", op),
+		"data":    data,
+		"code":    http.StatusOK,
+	})
+}
